da32d79d10c37564bf2432824ba24902e33f929b
Jada D <89225031+s0lcr@users.noreply.github.com>
2021-11-29 14:52:25 -0500

    v0.2
    Setup window, colors, font, and text.

--- .github/MyFirstPyGame.py
+++ .github/MyFirstPyGame.py
@@ -1,2 +1,33 @@
-# My First Pygame, Jada Dantzler, 11/29/21 2:28, v0.0
+# My First Pygame, Jada Dantzler, 11/29/21 2:28, v0.1
 
+import pygame, sys
+from pygame.locals import *
+
+# Start PyGame
+pygame.init()
+
+# Setup our window.
+windowSurface = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption('Hello, world!')
+
+# Setup Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 225)
+
+# Setup font.
+basicFont = pygame.font.SysFont(None, 48)
+
+# Setup text.
+text = basicFont.render('Hello, world!', True, WHITE, BLUE)
+textRect = text.get_rect()
+textRect.centerx = windowSurface.get_rect().centerx
+textRect.centery = windowSurface.get_rect().centery
+
+# Fill background color.
+windowSurface.fill(WHITE)
+
+# Draw a polygon onto the screen.
+pygame.draw.polygon(windowSurface, BLUE, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106))
