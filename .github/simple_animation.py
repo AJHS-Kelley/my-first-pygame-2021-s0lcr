706ce40febff6efe833eb680ee334a74a7bd9485
s0lcr <89225031+s0lcr@users.noreply.github.com>
2022-01-07 12:16:23 -0500

    v0.4
    Created the box values.

--- .github/simple_animation.py
+++ .github/simple_animation.py
@@ -1,4 +1,4 @@
-# Simple Animation with PyGame, Jada Dantzler, 01/07/22, 12:02AM, v0.3
+# Simple Animation with PyGame, Jada Dantzler, 01/07/22, 12:14AM, v0.4
 
 import pygame, sys, time
 from pygame.locals import *
@@ -11,6 +11,7 @@ WINDOWWIDTH = 400
 WINDOWHEIGHT = 400
 windowsurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
 pygame.display.set_caption('Animation Example!')
+
 
 # Setup the direction variables.
 DOWNLEFT = 'downleft'
@@ -25,3 +26,9 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN =(0, 255, 0)
 BLUE = (0, 0, 255)
+
+# Setup the box data.
+b1 = {'rect':pygame.Rect(300, 80, 50, 100), 'color':RED, 'dir':UPRIGHT}
+b2 = {'rect':pygame.Rect(200, 200, 20, 20), 'color':GREEN, 'dir':UPLEFT}
+b3 = {'rect':pygame.Rect(100, 150, 60, 60), 'color':BLUE, 'dir':DOWNLEFT}
+boxes = [b1, b2, b3]
