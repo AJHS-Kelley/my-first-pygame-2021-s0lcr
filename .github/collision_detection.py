3d230c1c74238b80d6256e7933046d6c3e5fa176
s0lcr <89225031+s0lcr@users.noreply.github.com>
2022-01-19 14:34:03 -0500

    v0.1
    Imported the pygame, system, and random modules.
    
    Setup PyGame and created mainClock variable.

--- .github/collision_detection.py
+++ .github/collision_detection.py
@@ -1,1 +1,8 @@
 # Pygame Collision Detection Practice, Jada Dantzler, January 19, 2011, 1:47pm, v0.0
+
+import pygame, sys, random
+from pygame.locals import *
+
+# Setup PyGame
+pygame.init()
+mainClock = pygame.time.Clock()
